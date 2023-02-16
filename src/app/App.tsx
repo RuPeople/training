f5b6a53a35bf7bc5a84945148c4fc2fedf06966d
David Chamurliev <d.chamurliev@taptima.ru>
2023-02-17 00:48:00 +1000

    TRN-2 Implement FSD architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,23 @@
 import { Suspense } from "react";
-import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import './styles/index.scss'
 
 const App = () => {
     const { theme, handleSwitchTheme } = useTheme()
 
     return (
-        <div className={classNames('app', {}, [])}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={handleSwitchTheme}>Свет</button>
             <Link to="/">Главная</Link>
             <Link to="/about">О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path="/about" element={<AboutPageAsync />} />
-                    <Route path="/" element={<MainPageAsync />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/" element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
