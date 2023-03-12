087e92ae7e95f76064102b5d00ce8be9281a9f81
David Chamurliev <d.chamurliev@taptima.ru>
2023-03-12 15:00:36 +1000

    TRN-8 Implement babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,29 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const babelLoader = {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: [
+                                "en",
+                                "ru"
+                            ],
+                            keyAsDefaultValue: true
+                        }
+                    ]
+                ],
+            }
+        }
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -46,6 +69,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     }
 
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
